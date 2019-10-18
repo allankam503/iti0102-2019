7a4551e81b63cfb81003587dbdc775cfcd13f2e4
Andrei Kashin <andrei.kashin@live.com>
2019-10-18 14:29:52 +0300

    Task: implement method using FRO and IF constructions.

--- test1.py
+++ test1.py
@@ -54,6 +54,28 @@ def test_for():
     X = [1,2,3,4,5]
     for x in "john doe":
         print(x + str(5))
+
+
+
+
+
+def get_even_numbers(numbers: list) -> list:
+    """
+    method returns only even elements of the given collection
+
+    >>> get_even_numbers([1,2,3,4,5] -> [2,4]
+    >>> get_even_numbers([2,4,6] -> [2,4,6]
+
+    :param numbers: List of integers
+    :return: List of only even integers from `numbers` in the same sequence 
+    """
+
+    # for ... in ...:
+    #     if ...:
+    #         .append(some_even_number)
+    #
+    # return result
+    pass
     
 if __name__ == '__main__':
     A = [1,2,3,10]
