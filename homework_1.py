7d4e33e579f1542e20333cd7b0edff3128f89578
Andy <andrei.kashin@gmail.com>
2019-11-10 09:11:40 +0200

    updated hw1 file

---
+++ homework_1.py
@@ -0,0 +1,24 @@
+def get_even_numbers(numbers: list) -> list:
+    """
+    method returns only even elements of the given collection
+
+    >>> get_even_numbers([1,2,3,4,5] -> [2,4]
+    >>> get_even_numbers([2,4,6] -> [2,4,6]
+
+    :param numbers: List of integers
+    :return: List of only even integers from `numbers` in the same sequence 
+    """
+
+    # for ... in ...:
+    #     if ...:
+    #         .append(some_even_number)
+    #
+    # return result
+    pass
+    
+if __name__ == '__main__':
+    A = [1,2,3,10]
+    B = [4,5,6]
+   
+    print(get_even_numbers(A))
+    print(get_even_numbers(B))    
